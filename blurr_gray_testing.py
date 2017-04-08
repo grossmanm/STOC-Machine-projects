8e82468aa0c069003f7aa3ab8905e5cb1237f354
Dancinggremlin <malcolmcgrossman@gmail.com>
2017-04-08 11:09:06 -0500

    woop

--- blurr_gray_testing.py
+++ blurr_gray_testing.py
@@ -11,6 +11,7 @@ while(True):
     img = cv2.bilateralFilter(frame, 15, 75, 75)
     imgg = cv2.cvtColor(frame, cv2.COLOR_RGBA2GRAY )
     imggg = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # I tried so hard and got so far, but in the end I am just the smoking man from X-files
     cv2.imshow("Blur Testing", img)
     #cv2.imshow("No Blur", frame)
     cv2.imshow("W/ Alpha Channel", imgg)
